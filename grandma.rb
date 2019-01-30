005061c34151c0d15be0360135bc8c1f72765991
Vytautas Klimavicius <vyklimavicius@gmail.com>
2019-01-30 16:28:27 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,9 +1,9 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(phrase)
  if phrase == phrase.upcase
-   puts "I LOVE YOU TOO PUMPKIN!"
+   "I LOVE YOU TOO PUMPKIN!"
  else 
-   puts "HUH?! SPEAK UP, SONNY!"
+   "HUH?! SPEAK UP, SONNY!"
  end
 end
 
