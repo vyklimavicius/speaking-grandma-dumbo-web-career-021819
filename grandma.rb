efd40d28ecb0af08409ca293edcc6ee710b9f29b
Vytautas Klimavicius <vyklimavicius@gmail.com>
2019-01-30 16:37:24 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,8 +1,10 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(phrase)
  if phrase == phrase.upcase
-   "I LOVE YOU TOO PUMPKIN!"
- else 
+   "NO, NOT SINCE 1938!"
+  elsif phrase == "I LOVE YOU GRANDMA!"
+    "I LOVE YOU TOO PUMPKIN!"
+  else 
    "HUH?! SPEAK UP, SONNY!"
  end
 end
